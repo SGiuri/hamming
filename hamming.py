28fe082123e4544823a63fff114dd23cb3bd6f3a
SGiuri <s.giuri@issm.it>
2020-09-15 14:49:31 +0200

    Done

--- hamming.py
+++ hamming.py
@@ -1,2 +1,18 @@
 def distance(strand_a, strand_b):
-    pass
+
+    # Check equal Lenght
+
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Equal length not verified")
+
+    # set up a counter of differences
+    hamming_difference = 0
+
+    # checking each letter
+    for j in range(len(strand_a)):
+        # if there is a difference, increasing the hamming difference by 1
+        if strand_a[j] != strand_b[j]:
+            hamming_difference += 1
+
+    return hamming_difference
+
